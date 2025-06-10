f9f338e0e0c478005534d42fd56963c462f0d70d
Basicgit <god0999333@gmail.com>
2025-06-10 14:29:51 +0530

    first commit

--- ourproject/settings.py
+++ ourproject/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s8n5w8gtxe(_##79-kgu9j8&(h%$mh#eguuzqy!k$k6x=#fw0('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,7 +131,13 @@ LOGIN_URL='login'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 # Looking to send emails in production? Check out our Email API/SMTP product!
-EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
-EMAIL_HOST_USER = '0206145d901f79'
-EMAIL_HOST_PASSWORD = '7452221a3cb505'
-EMAIL_PORT = '2525'
+# EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+# EMAIL_HOST_USER = '0206145d901f79'
+# EMAIL_HOST_PASSWORD = '7452221a3cb505'
+# EMAIL_PORT = '2525'
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
